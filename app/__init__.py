4b0044ee9753fabc649e293ad2c70b39e57c4859
星龙剑 <xinglongjian@qq.com>
2017-12-24 00:29:47 +0000

    update manage.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,11 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
 from config import config
-
-db = SQLAlchemy()
 
 def create_app(config_name):
     """create app"""
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    
-    db.init_app(app)
-    db.create_all(app=app)
     
     #register blueprint
     from app.api import api as api_blueprint
--- manage.py
+++ manage.py
@@ -1,23 +1,30 @@
 # -*- coding: UTF-8 -*-
 import os
+from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager, Shell, Server
-from app import create_app,db
+from flask_migrate import Migrate,MigrateCommand
+from app import create_app
+from app.models import *
 from log import *
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-app.app_context().push()
-#create tables
+# app.app_context().push()
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+def createall():
+    db.create_all()
 
 manager = Manager(app)
 manager.add_command('runserver', Server(use_debugger=True))
+manager.add_command("db", MigrateCommand)
+manager.add_command("dbcreate", createall())
 
-@manager.command
-def make_shell_context():
-    return dict(app=app, db=db)
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
+# @manager.command
+# def make_shell_context():
+#     return dict(app=app, db=db)
+# manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     logging.info('start server...')
-    manager.run(default_command='runserver')
+    manager.run()
